84d2fc3115bae9143e9e69361b29b87594d3e567
TerrainLife <embedded.life@yandex.ru>
2019-07-31 15:51:11 +0300

    Start of refactoring to multiple hosts

--- rtptest.go
+++ rtptest.go
@@ -17,20 +17,28 @@ var patternsMap = map[string][]byte{
 	"1000Hz": []byte{0xD5, 0x23, 0x2A, 0x23, 0xD5, 0xA3, 0xAA, 0xA3},
 }
 
-var localPort = 20002
-var local, _ = net.ResolveIPAddr("ip", "172.20.32.3")
+type host struct {
+	ip   string
+	port int
+}
 
-var remotePort = 10000
-var remote, _ = net.ResolveIPAddr("ip", "172.20.32.200")
+// var ssrc uint32 = 0xdeadbeef
+var ssrc uint32 = 0x11223344
 
-var localZone = ""
-var remoteZone = ""
+func fillPatternMap() {
+	for key, pattern := range patternsMap {
+		var tmp []byte
+		for i := 0; i < payloadSize/len(pattern); i++ {
+			tmp = append(tmp, pattern...)
+		}
 
-var rtpSession *rtp.Session
+		patternsMap[key] = tmp
 
-var ssrc uint32 = 0xdeadbeef
+		printBuf(patternsMap[key])
+	}
+}
 
-func receiveFn(done <-chan struct{}, wg *sync.WaitGroup) {
+func receiveFn(rtpSession *rtp.Session, done <-chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	// Create and store the data receive channel.
@@ -52,20 +60,7 @@ func receiveFn(done <-chan struct{}, wg *sync.WaitGroup) {
 
 }
 
-func fillPatternMap() {
-	for key, pattern := range patternsMap {
-		var tmp []byte
-		for i := 0; i < payloadSize/len(pattern); i++ {
-			tmp = append(tmp, pattern...)
-		}
-
-		patternsMap[key] = tmp
-
-		printBuf(patternsMap[key])
-	}
-}
-
-func senderFn(done <-chan struct{}, wg *sync.WaitGroup) {
+func senderFn(rtpSession *rtp.Session, done <-chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	ticker := time.NewTicker(20 * time.Millisecond)
@@ -101,32 +96,45 @@ func printBuf(buf []byte) {
 	fmt.Println("buf len=%u", len(buf))
 }
 
-func main() {
-	fmt.Println("RTP test start!")
-
-	fillPatternMap()
-
+func initRtpSession(local host, remote ...host) {
 	var wg sync.WaitGroup
 	done := make(chan struct{}, 2)
 	defer close(done)
 
+	localHost, err := net.ResolveIPAddr("ip", local.ip)
+	if err != nil {
+		fmt.Printf("Resolve Local address %s FAIL:%s", local.ip, err.Error())
+		return
+	}
+
 	// Create a UDP transport with "local" address and use this for a "local" RTP session
 	// The RTP session uses the transport to receive and send RTP packets to the remote peer.
-	transport, _ := rtp.NewTransportUDP(local, localPort, localZone)
+	transport, err := rtp.NewTransportUDP(localHost, local.port, "")
+	if err != nil {
+		fmt.Printf("Can not create %s:%d Transport layer:%s", local.ip, local.port, err.Error())
+		return
+	}
 
 	// TransportUDP implements TransportWrite and TransportRecv interfaces thus
 	// use it to initialize the Session for both interfaces.
-	rtpSession = rtp.NewSession(transport, transport)
+	rtpSession := rtp.NewSession(transport, transport)
 
-	// Add address of a remote peer (participant)
-	rtpSession.AddRemote(&rtp.Address{remote.IP, remotePort, remotePort + 1, remoteZone})
+	for _, remoteHost := range remote {
+		remoteHostConverted, err := net.ResolveIPAddr("ip", remoteHost.ip)
+		if err != nil {
+			fmt.Printf("Resolve Remote address %s FAIL:%s", remoteHost.ip, err.Error())
+			continue
+		}
+		// Add address of a remote peer (participant)
+		rtpSession.AddRemote(&rtp.Address{remoteHostConverted.IP, remoteHost.port, remoteHost.port + 1, ""})
+	}
 
 	// Create a media stream.
 	// The SSRC identifies the stream. Each stream has its own sequence number and other
 	// context. A RTP session can have several RTP stream for example to send several
 	// streams of the same media.
 	//
-	outStreamIdx, _ := rtpSession.NewSsrcStreamOut(&rtp.Address{local.IP, localPort, localPort + 1, localZone}, ssrc, 1)
+	outStreamIdx, _ := rtpSession.NewSsrcStreamOut(&rtp.Address{localHost.IP, local.port, local.port + 1, ""}, 0, 0)
 	rtpSession.SsrcStreamOutForIndex(outStreamIdx).SetPayloadType(8)
 
 	wg.Add(1)
@@ -144,5 +152,14 @@ func main() {
 	wg.Wait()
 
 	rtpSession.CloseSession()
+}
+
+func main() {
+	fmt.Println("RTP test start!")
+
+	fillPatternMap()
+
+	var wg sync.WaitGroup
+
 	fmt.Println("RTP test stop!")
 }
